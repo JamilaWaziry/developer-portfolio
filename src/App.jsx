54210754a264c330685e813cf263d6220fb2932e
JamilaWaziry <jamilawaziry07@gmail.com>
2026-05-25 22:39:23 +0430

    recent changes

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import Profile from "./components/Profile";
 import About from "./components/About";
 import Projects from "./components/Projects";
 import Footer from "./components/Footer";
-import profilePic from "./assets/photo1.jpg";
+import profilePic from "./assets/logo.jpg";
 import "./styles/global.css";
 
 export default function App() {
@@ -12,11 +12,13 @@ export default function App() {
     <div>
       <Navbar />
       <Header message="Bring your ideas to life with code!" />
-      <Profile
-        photo={profilePic}
-        title="Frontend Developer"
-        bio="I love building responsive web apps."
-      />
+      <div className="App">
+        <Profile
+          photo={profilePic}
+          title="Frontend Developer"
+          bio="Frontend Developer driven by curiosity, creativity, and code. I enjoy crafting sleek user interfaces, solving real problems, and building web experiences that are both visually engaging and functional."
+        />
+      </div>
       <About />
       <Projects />
       <Footer />
--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -1,11 +1,53 @@
 import "../styles/profile.css";
 
+import { FaReact, FaGithub, FaCss3Alt, FaNodeJs } from "react-icons/fa";
+
+import { SiJavascript, SiHtml5, SiPython } from "react-icons/si";
+
 export default function Profile({ photo, title, bio }) {
   return (
     <section className="profile">
-      <img src={photo} alt="Profile" />
-      <h2>{title}</h2>
-      <p>{bio}</p>
+      <div className="profile-left">
+        <div className="profile-info">
+          <img src={photo} alt="Profile" className="profile-pic" />
+
+          <div className="profile-text">
+            <h2>{title}</h2>
+
+            <p>{bio}</p>
+          </div>
+        </div>
+      </div>
+
+      <div className="profile-right">
+        <div className="circle">
+          <FaReact className="react-center" />
+
+          <SiJavascript className="big-icon js-icon" />
+
+          <FaNodeJs className="big-icon node-icon" />
+
+          <FaGithub className="big-icon github-icon" />
+
+          <SiHtml5 className="big-icon html-icon" />
+
+          <FaCss3Alt className="big-icon css-icon" />
+
+          <SiPython className="big-icon python-icon" />
+
+          <span className="small-icon symbol1">{"{}"}</span>
+
+          <span className="small-icon symbol2">{"<>"}</span>
+
+          <span className="small-icon symbol3">==</span>
+
+          <span className="small-icon symbol4">!==</span>
+
+          <span className="small-icon symbol5">#</span>
+
+          <span className="small-icon symbol6">λ</span>
+        </div>
+      </div>
     </section>
   );
 }
